d8f4e92effba3f5bc9c10bd4c0671fdd8b054f49
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-06-17 11:27:53 +0900

    x.erl  add some sanity checks

--- ryu/tests/unit/ofproto/mkdata/src/x.erl
+++ ryu/tests/unit/ofproto/mkdata/src/x.erl
@@ -8,9 +8,20 @@ do(Body, N) ->
     OFPVersion = 3,
     Name = atom_to_list(element(1, Body)),
     io:format("processing ~B ~B ~s~n", [OFPVersion, N, Name]),
-    Msg = ofp_v3_encode:do(#ofp_message{version=OFPVersion, xid=0, body=Body}),
+    M = #ofp_message{version=OFPVersion, xid=0, body=Body},
+    Msg = ofp_v3_encode:do(M),
     {ok, F} = file:open(["../data/", integer_to_list(OFPVersion), "-",
         integer_to_list(N), "-", Name, ".packet"], [write, binary]),
+
+    % sanity check
+    % this is fragile because of order of flags.
+    % ofp flags are unorderd but of_protocol keeps them in a list.
+    {ok, M2, <<>>} = ofp_v3_decode:do(Msg),
+    #ofp_message{version=OFPVersion, type=_, xid=0, body=Body2} = M2,
+    io:format("~p~n", [Body]),
+    io:format("~p~n", [Body2]),
+    Body = Body2,
+
     ok = file:write(F, Msg),
     ok = file:close(F),
     N + 1.
