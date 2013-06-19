fd62305a554aabe35dffbeaeb8d88846616206e1
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-06-19 09:13:49 +0900

    x.erl  cleanup

--- ryu/tests/unit/ofproto/mkdata/src/x.erl
+++ ryu/tests/unit/ofproto/mkdata/src/x.erl
@@ -7,21 +7,21 @@
 do(Body, {OFPVersion, N}) ->
     Name = atom_to_list(element(1, Body)),
     io:format("processing ~B ~B ~s~n", [OFPVersion, N, Name]),
-    M = #ofp_message{version=OFPVersion, xid=0, body=Body},
-    Msg = ofp_v3_encode:do(M),
+    Msg = #ofp_message{version=OFPVersion, xid=0, body=Body},
+    {ok, BinMsg} = of_protocol:encode(Msg),
     {ok, F} = file:open(["../data/", integer_to_list(OFPVersion), "-",
         integer_to_list(N), "-", Name, ".packet"], [write, binary]),
 
     % sanity check
     % this is fragile because of order of flags.
     % ofp flags are unorderd but of_protocol keeps them in a list.
-    {ok, M2, <<>>} = of_protocol:decode(Msg),
-    #ofp_message{version=OFPVersion, type=_, xid=0, body=Body2} = M2,
+    {ok, Msg2, <<>>} = of_protocol:decode(BinMsg),
+    #ofp_message{version=OFPVersion, type=_, xid=0, body=Body2} = Msg2,
     % io:format("~p~n", [Body]),
     % io:format("~p~n", [Body2]),
     Body = Body2,
 
-    ok = file:write(F, Msg),
+    ok = file:write(F, BinMsg),
     ok = file:close(F),
     {OFPVersion, N + 1}.
 
