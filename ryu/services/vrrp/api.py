8731a8799129c38f205ee6c0978e2d9398f1d18c
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-07-17 13:31:09 +0900

    vrrp.api: comment

--- ryu/services/vrrp/api.py
+++ ryu/services/vrrp/api.py
@@ -19,6 +19,10 @@ from ryu.services.vrrp import event as vrrp_event
 
 def vrrp_config(app, interface, config):
     """create an instance.
+    returns EventVRRPConfigReply(instance.name, interface, config)
+    on success.
+    returns EventVRRPConfigReply(None, interface, config)
+    on failure.
     """
     config_request = vrrp_event.EventVRRPConfigRequest(interface, config)
     config_request.sync = True
