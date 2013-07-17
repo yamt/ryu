bcbbc6592c7ac279ba2aed341e89a6e1fbe28432
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-07-17 13:05:17 +0900

    vrrp.monitor: comment

--- ryu/services/vrrp/monitor.py
+++ ryu/services/vrrp/monitor.py
@@ -128,6 +128,12 @@ class VRRPInterfaceMonitor(app_manager.RyuApp):
         # it ourselvs.
         packet_ = packet.Packet(packet_data)
         protocols = packet_.protocols
+
+        # we expect either of
+        #   [ether, vlan, ip, vrrp{, padding}]
+        # or
+        #   [ether, ip, vrrp{, padding}]
+
         if len(protocols) < 2:
             self.logger.debug('len(protocols) %d', len(protocols))
             return
