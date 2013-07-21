9b22e3890977ad738d12e1b4796a4dc3a3a19ba0
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-07-22 08:42:37 +0900

    stop sharing oxm_types among OF versions
    this restores the previous situation.
    ie. don't provide PBB definitions etc for OF1.2.

--- ryu/ofproto/oxm_fields.py
+++ ryu/ofproto/oxm_fields.py
@@ -82,7 +82,7 @@ class IPv6Addr(TypeDescr):
 OFPXMC_OPENFLOW_BASIC = 0x8000
 
 
-class D(object):
+class OpenFlowBasic(object):
     _class = OFPXMC_OPENFLOW_BASIC
 
     def __init__(self, name, num, type_):
@@ -91,60 +91,17 @@ class D(object):
         self.type = type_
 
 
-oxm_types = [
-    D('in_port', 0, Int4),
-    D('in_phy_port', 1, Int4),
-    D('metadata', 2, Int8),
-    D('eth_dst', 3, MacAddr),
-    D('eth_src', 4, MacAddr),
-    D('eth_type', 5, Int2),
-    D('vlan_vid', 6, Int2),
-    D('vlan_pcp', 7, Int1),
-    D('ip_dscp', 8, Int1),
-    D('ip_ecn', 9, Int1),
-    D('ip_proto', 10, Int1),
-    D('ipv4_src', 11, IPv4Addr),
-    D('ipv4_dst', 12, IPv4Addr),
-    D('tcp_src', 13, Int2),
-    D('tcp_dst', 14, Int2),
-    D('udp_src', 15, Int2),
-    D('udp_dst', 16, Int2),
-    D('sctp_src', 17, Int2),
-    D('sctp_dst', 18, Int2),
-    D('icmpv4_type', 19, Int1),
-    D('icmpv4_code', 20, Int1),
-    D('arp_op', 21, Int2),
-    D('arp_spa', 22, IPv4Addr),
-    D('arp_tpa', 23, IPv4Addr),
-    D('arp_sha', 24, MacAddr),
-    D('arp_tha', 25, MacAddr),
-    D('ipv6_src', 26, IPv6Addr),
-    D('ipv6_dst', 27, IPv6Addr),
-    D('ipv6_flabel', 28, Int4),
-    D('icmpv6_type', 29, Int1),
-    D('icmpv6_code', 30, Int1),
-    D('ipv6_nd_target', 31, IPv6Addr),
-    D('ipv6_nd_sll', 32, MacAddr),
-    D('ipv6_nd_tll', 33, MacAddr),
-    D('mpls_label', 34, Int4),
-    D('mpls_tc', 35, Int1),
-    D('mpls_bos', 36, Int1),
-    D('pbb_isid', 37, Int3),
-    D('tunnel_id', 38, Int8),
-    D('ipv6_exthdr', 39, Int2),
-]
-
-
-def generate_constants(modname):
+def generate(modname):
     import sys
     import string
+    import functools
 
     mod = sys.modules[modname]
 
     def add_attr(k, v):
         setattr(mod, k, v)
 
-    for i in oxm_types:
+    for i in mod.oxm_types:
         uk = string.upper(i.name)
         oxm_class = i.num >> 7
         if oxm_class != OFPXMC_OPENFLOW_BASIC:
@@ -155,11 +112,14 @@ def generate_constants(modname):
         add_attr('OXM_OF_' + uk, mod.oxm_tlv_header(ofpxmt, td.size))
         add_attr('OXM_OF_' + uk + '_W', mod.oxm_tlv_header_w(ofpxmt, td.size))
 
+    name_to_field = dict((f.name, f) for f in mod.oxm_types)
+    num_to_field = dict((f.num, f) for f in mod.oxm_types)
+    add_attr('oxm_from_user', functools.partial(from_user, name_to_field))
+    add_attr('oxm_to_user', functools.partial(to_user, num_to_field))
+    add_attr('oxm_normalize_user', functools.partial(normalize_user, mod))
 
-name_to_field = dict((f.name, f) for f in oxm_types)
 
-
-def from_user(name, user_value):
+def from_user(name_to_field, name, user_value):
     f = name_to_field[name]
     t = f.type
     if isinstance(user_value, tuple):
@@ -173,10 +133,7 @@ def from_user(name, user_value):
     return f.num, value, mask
 
 
-num_to_field = dict((f.num, f) for f in oxm_types)
-
-
-def to_user(n, v, m):
+def to_user(num_to_field, n, v, m):
     f = num_to_field[n]
     t = f.type
     value = t.to_user(v)
@@ -187,12 +144,12 @@ def to_user(n, v, m):
     return f.name, user_value
 
 
-def normalize_user(k, uv):
-    (n, v, m) = from_user(k, uv)
+def normalize_user(mod, k, uv):
+    (n, v, m) = mod.oxm_from_user(k, uv)
     # apply mask
     if not m is None:
         v = ''.join(chr(ord(x) & ord(y)) for (x, y)
             in itertools.izip(v, m))
-    (k2, uv2) = to_user(n, v, m)
+    (k2, uv2) = mod.oxm_to_user(n, v, m)
     assert k2 == k
     return (k2, uv2)
