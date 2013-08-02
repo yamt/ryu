696295babcdbd0729cd5175fae40cbbd1f433d50
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-08-02 15:51:49 +0900

    vrrp test improvements
    - comments
    - share code between tests
    - actually check status of instances
    - use many VRIDs on an interface
    - test dynamic priority changes
    - reduce test time
    - update after the recent api changes
    
    test_vrrp_multi  avoid picking OVS local port
    
    test_vrrp_multi: comment
    
    test_vrrp_multi  be a little more deterministic
    
    test_vrrp_linux_multi: comments
    
    test_vrrp_linux_multi  assert states
    
    test_vrrp_linux_multi  create many virtual routers
    
    test_vrrp_linux_multi  reduce test time.  more checks.
    
    test_vrrp_linux_multi: fix type of primary_ip_address
    
    test_vrrp_linux_multi: remove commented out code
    
    test_vrrp_linux_multi: remove unused _IP_ADDRESS
    
    test_vrrp_linux_multi: update after the recent api changes
    
    vrrp tests  catch up with the recent api changes
    
    share code among vrrp tests
    
    tests.integrated.vrrp_common  comments whitespace
    
    vrrp_common  fix a message
    
    vrrp_common  move checking code into a method
    
    vrrp_common  shuffle priority

--- ryu/tests/integrated/test_vrrp_multi.py
+++ ryu/tests/integrated/test_vrrp_multi.py
@@ -19,14 +19,16 @@ Usage:
 PYTHONPATH=. ./bin/ryu-manager --verbose \
              ./ryu/topology/switches.py \
              ./ryu/services/vrrp/manager.py \
-             ./ryu/tests/integrated/test_vrrp.py \
+             ./ryu/tests/integrated/test_vrrp_multi.py \
              ./ryu/services/vrrp/dumper.py
 
 ./ryu/services/vrrp/dumper.py is optional.
 
-  -----          --------------          -----
-  |OVS|<--veth-->|Linux bridge|<--veth-->|OVS|
-  -----          --------------          -----
+         +---+          ----------------
+      /--|OVS|<--veth-->|              |
+   Ryu   +---+          | linux bridge |<--veth--> command to generate packets
+      \--|OVS|<--veth-->|              |
+         +---+          ----------------
 
 configure OVSs to connect ryu
 example
@@ -76,22 +78,24 @@ ovs-system              0000.122038293b55       no
 # ip link b0 set up
 """
 
-import netaddr
-import time
-
 from ryu.base import app_manager
 from ryu.controller import handler
 from ryu.lib import dpid as lib_dpid
 from ryu.lib import hub
-from ryu.lib import mac as lib_mac
+from ryu.lib import addrconv
 from ryu.lib.packet import vrrp
 from ryu.services.vrrp import api as vrrp_api
 from ryu.services.vrrp import event as vrrp_event
 from ryu.topology import event as topo_event
 from ryu.topology import api as topo_api
 
+from . import vrrp_common
 
-class VRRPConfigApp(app_manager.RyuApp):
+
+class VRRPConfigApp(vrrp_common.VRRPCommon):
+    _IFNAME0 = 0
+    _IFNAME1 = 1
+
     def __init__(self, *args, **kwargs):
         super(VRRPConfigApp, self).__init__(*args, **kwargs)
         self.start_main = False
@@ -106,84 +110,35 @@ class VRRPConfigApp(app_manager.RyuApp):
             return
 
         self.start_main = True
+        app_mgr = app_manager.AppManager.get_instance()
+        self.logger.debug('%s', app_mgr.applications)
+        self.switches = app_mgr.applications['switches']
         hub.spawn(self._main)
 
-    def _main(self):
-        time.sleep(1)
-        self.logger.debug('########## test start ##########')
-        self._main_version(vrrp.VRRP_VERSION_V3)
-        time.sleep(5)
-        self._main_version(vrrp.VRRP_VERSION_V2)
-        self.logger.debug('########## test done ##########')
-
-    def _main_version(self, vrrp_version):
-        self._main_version_priority(vrrp_version,
-                                    vrrp.VRRP_PRIORITY_ADDRESS_OWNER)
-        time.sleep(5)
-        self._main_version_priority(vrrp_version,
-                                    vrrp.VRRP_PRIORITY_BACKUP_MAX)
-        time.sleep(5)
-        self._main_version_priority(vrrp_version,
-                                    vrrp.VRRP_PRIORITY_BACKUP_DEFAULT)
-        time.sleep(5)
-        self._main_version_priority(vrrp_version,
-                                    vrrp.VRRP_PRIORITY_BACKUP_MIN)
-
-    def _main_version_priority(self, vrrp_version, priority):
-        self._main_version_priority_sleep(vrrp_version, priority, False)
-        time.sleep(5)
-        self._main_version_priority_sleep(vrrp_version, priority, True)
-
-    def _config_switch(self, switches, switch_index,
-                       vrrp_version, ip_addr, priority):
+    def _configure_vrrp_router(self, vrrp_version, priority,
+                               ip_addr, switch_index, vrid):
+        switches = self.switches
         self.logger.debug('%s', switches.dps)
-        dpid = switches.dps.keys()[switch_index]
+        dpid = sorted(switches.dps.keys())[switch_index]
         self.logger.debug('%s', lib_dpid.dpid_to_str(dpid))
         self.logger.debug('%s', switches.port_state)
-        port_no = switches.port_state[dpid].keys()[0]
+        # hack: use the smallest port no to avoid picking OVS local port
+        port_no = sorted(switches.port_state[dpid].keys())[0]
         self.logger.debug('%d', port_no)
         port = switches.port_state[dpid][port_no]
         self.logger.debug('%s', port)
-        mac = port.hw_addr
-        self.logger.debug('%s', lib_mac.haddr_to_str(mac))
+        mac = addrconv.mac.bin_to_text(port.hw_addr)
+        self.logger.debug('%s', mac)
 
-        ip_addr = netaddr.IPAddress(ip_addr).value
         interface = vrrp_event.VRRPInterfaceOpenFlow(
             mac, ip_addr, None, dpid, port_no)
         self.logger.debug('%s', interface)
 
         config = vrrp_event.VRRPConfig(
-            version=vrrp_version, vrid=7, priority=priority,
+            version=vrrp_version, vrid=vrid, priority=priority,
             ip_addresses=[ip_addr])
         self.logger.debug('%s', config)
 
         rep = vrrp_api.vrrp_config(self, interface, config)
         self.logger.debug('%s', rep)
         return rep
-
-    def _main_version_priority_sleep(self, vrrp_version, priority, do_sleep):
-        self.logger.debug('########## '
-                          'test vrrp_verson %s priority %d do_sleep %d '
-                          '##########',
-                          vrrp_version, priority, do_sleep)
-        app_mgr = app_manager.AppManager.get_instance()
-        self.logger.debug('%s', app_mgr.applications)
-        vrrp_mgr = app_mgr.applications['VRRPManager']
-        switches = app_mgr.applications['switches']
-
-        rep1 = self._config_switch(switches, 1, vrrp_version, '10.0.0.2',
-                                   vrrp.VRRP_PRIORITY_BACKUP_DEFAULT)
-        if do_sleep:
-            time.sleep(5)
-        rep0 = self._config_switch(switches, 0, vrrp_version, '10.0.0.1',
-                                   priority)
-
-        self.logger.debug('%s', vrrp_mgr._instances)
-
-        if do_sleep:
-            time.sleep(10)
-
-        vrrp_api.vrrp_shutdown(self, rep0.instance_name)
-        if do_sleep:
-            time.sleep(10)
-        vrrp_api.vrrp_shutdown(self, rep1.instance_name)
