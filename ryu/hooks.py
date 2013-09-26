f851cfe4fa78740f6ca1b3af36440a2cd688a3a4
yamamoto <yamt@mwd.biglobe.ne.jp>
2013-09-26 15:36:35 +0900

    fix ordering constraint between setup hooks

--- ryu/hooks.py
+++ ryu/hooks.py
@@ -17,7 +17,23 @@
 
 import os
 import sys
+from setuptools.command import easy_install
 from ryu import version
+
+
+# Global variables in this module doesn't work as we expect
+# because, during the setup procedure, this module seems to be
+# copied (as a file) and can be loaded multiple times.
+# We save them into __main__ module instead.
+def _main_module():
+    return sys.modules['__main__']
+
+
+def save_orig():
+    """Save original easy_install.get_script_args.
+    This is necessary because pbr's setup_hook is sometimes called
+    before ours."""
+    _main_module()._orig_get_script_args = easy_install.get_script_args
 
 
 def setup_hook(config):
@@ -33,16 +49,17 @@ def setup_hook(config):
 
     metadata['version'] = str(version)
 
-    # pbr's setup_hook which will be executed after us replaces
-    # easy_install.get_script_args with their own version,
-    # override_get_script_args, prefering simpler scripts which
-    # are not aware of multi-version.
+    # pbr's setup_hook replaces easy_install.get_script_args with
+    # their own version, override_get_script_args, prefering simpler
+    # scripts which are not aware of multi-version.
     # prevent that by doing the opposite.  it's a horrible hack
     # but we are in patching wars already...
     from pbr import packaging
-    from setuptools.command import easy_install
 
-    packaging.override_get_script_args = easy_install.get_script_args
+    def my_get_script_args(*args, **kwargs):
+        return _main_module()._orig_get_script_args(*args, **kwargs)
+
+    packaging.override_get_script_args = my_get_script_args
 
     # another hack to allow setup from tarball.
     orig_get_version = packaging.get_version
